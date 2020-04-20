312bb31db5c3b51e975a4e1efe8d724af375d724
Sean Doyle <sdoylelambda@gmail.com>
2020-04-20 17:31:41 -0400

    Gettin reps.

---
+++ untitled folder/negateArr.js
@@ -0,0 +1,9 @@
+// Given an array of numbers, negate all elements contained within.
+
+function negate(arr) {}
+
+negate([1, 2, 3, 4]) //➞ [-1, -2, -3, -4]
+
+negate([-1, 2, -3, 4]) //➞ [1, -2, 3, -4]
+
+negate([]) //➞ []
