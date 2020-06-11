2ee576e47c05d95d7d1e077eb383d95b2c0ac0c9
Sean Doyle <sdoylelambda@gmail.com>
2020-06-11 12:31:55 -0400

    New branch.

--- Edabit Medium/convertObjectToArray.js
+++ Edabit Medium/convertObjectToArray.js
@@ -1,5 +1,5 @@
 // Convert Key, Values in an Object to Array
-// Write a function that converts an object into an array of keys and values.
+// Write a function that converts an object into an array of keys and values .
 
 function objectToArray(obj) {
   const outputArr = []
