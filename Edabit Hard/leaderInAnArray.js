6d6799acd67f23ee4bafaff2f69da5a57c1585a9
Sean Doyle <sdoylelambda@gmail.com>
2020-10-18 18:29:53 -0400

    leaderInArray passing 3 of 4 tests.

--- Edabit Hard/leaderInAnArray.js
+++ Edabit Hard/leaderInAnArray.js
@@ -42,18 +42,18 @@
 function leader(arr) {
   const outputArr = []
   var largestNumber = Math.max(...arr)
-  console.log('largestNumber', largestNumber)
+  // console.log('largestNumber', largestNumber)
   var largestNumberPosition = arr.indexOf(Math.max(...arr))
-  console.log('largestNumberPosition', largestNumberPosition)
+  // console.log('largestNumberPosition', largestNumberPosition)
   outputArr.push(largestNumber)
-  arr.splice(largestNumberPosition, 1)
+  arr.splice(0, largestNumberPosition)
   var secondlargestNumber = Math.max(...arr)
-  console.log('secondlargestNumber', secondlargestNumber)
+  // console.log('secondlargestNumber', secondlargestNumber)
   var secondlargestNumberPosition = arr.indexOf(Math.max(...arr))
-  console.log('secondlargestNumberPosition', secondlargestNumberPosition + 1)
-  console.log('output = 15', arr)
+  // console.log('secondlargestNumberPosition', secondlargestNumberPosition + 1)
+  // console.log('output = 15', arr)
 // NOW THAT LARGEST NUM IS IN ARRAY
-while (arr.length > 1) {
+while (arr.length > 0) {
 // IF 2ND LARGEST NUM IS NEXT IT IT ADD IT
   if (largestNumberPosition === secondlargestNumberPosition) {
     var largestNumber = Math.max(...arr)
@@ -91,15 +91,7 @@ while (arr.length > 1) {
 }
 
 // Examples
-leader([2, 3, 20, 15, 8, 3]) //➞ [20, 15, 8, 3]
-
-
-
-
-
-
-
-
+// leader([2, 3, 20, 15, 8, 3]) //➞ [20, 15, 8, 3]
 // Note that 20 is greater than all the elements to it's
 // right side, similarly 15 and so on.
 
@@ -109,7 +101,7 @@ leader([2, 3, 20, 15, 8, 3]) //➞ [20, 15, 8, 3]
 // leader([1, 2, 3, 4, 5]) //➞ [5]
 // 5 is technically greater than the (zero) remaining items.
 
-// leader([8, 7, 1, 2, 10, 3, 5]) //➞[10, 5]
+leader([8, 7, 1, 2, 10, 3, 5]) //➞[10, 5]
 // 10 is greater than all items to the right of it, so include.
 // 3 is not greater than all items to the right of it, so exclude.
 // 5 is greater than the remaining items, so include.
