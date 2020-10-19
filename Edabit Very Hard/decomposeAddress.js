e4fe56af2d382e32b446e6f5b6620f5448c7dc5d
Sean Doyle <sdoylelambda@gmail.com>
2020-10-19 13:15:41 -0400

    decomposeAddress: passes all tests.

--- Edabit Very Hard/decomposeAddress.js
+++ Edabit Very Hard/decomposeAddress.js
@@ -25,7 +25,51 @@
 // ADD TO ARRAY
 // RETURN ARRAY
 
+// function decomposeAddress(str) {
+//   // CREATE OUTPUT ARRAY
+//   const outputArr = []
+//   // SET VARIABLES FOR VALUES
+//   var streetNumber = '557'
+//   var streetName = 'Farmer Rd'
+//   var cityName = 'Corner'
+//   var state = 'MT'
+//   var zipCode = '59105'
+
+//   const streetNumberRegEx = /^(\d+)/
+//   streetNumber = str.match(streetNumberRegEx)
+//   console.log('streetNumber:', streetNumber[0])
+ 
+//   const streetNameRegEx = /d/
+//   streetName = str.split(' ')
+//   console.log('streetName:', streetName)
+
+//   // const cityNameRegEx = /d/
+//   // cityName = str.match(cityNameRegEx)
+
+//   // const stateRegEx = /d/
+//   // state = str.match(stateRegEx)
+
+//   // const zipCodeRegEx = /d/
+//   // zipCode = str.match(zipCodeRegEx)
+
+//   // ADD TO ARRAY
+//   outputArr.push(streetNumber[0], streetName, cityName, state, zipCode)
+
+//   // RETURN ARRAY
+//   console.log('str:', str)
+//   console.log('output:', outputArr)
+
+//   return outputArr
+// }
+
+
+
+
+
 function decomposeAddress(str) {
+
+  const res = str.replace(/[, ]+/g, " ").trim();
+  console.log('res', res)
   // CREATE OUTPUT ARRAY
   const outputArr = []
   // SET VARIABLES FOR VALUES
@@ -35,31 +79,48 @@ function decomposeAddress(str) {
   var state = 'MT'
   var zipCode = '59105'
 
-  const streetNumberRegEx = /^(\d+)/
-  streetNumber = str.match(streetNumberRegEx)
-  console.log('streetNumber:', streetNumber[0])
+  var strArr = res.split(' ')
+
+  streetNumber = strArr[0]
+  streetName = strArr[1] + ' ' + strArr[2]
+  console.log('streetname:', streetName)
+  cityName = strArr[3]
+  state = strArr[4]
+  zipCode = strArr[5]
+  console.log('strArr:', strArr)
   
-  // const streetNameRegEx = /d/
-  // streetName = str.match(streetNameRegEx)
+  if (strArr.length === 6) {
+  
+  outputArr.push(streetNumber)
+  outputArr.push(streetName)
+  outputArr.push(cityName)
+  outputArr.push(state)
+  outputArr.push(zipCode)
+  } else {
 
-  // const cityNameRegEx = /d/
-  // cityName = str.match(cityNameRegEx)
+    streetNumber = strArr[0]
+    streetName = strArr[1] + ' ' + strArr[2]
+    console.log('streetname:', streetName)
+    cityName = strArr[3] + ' ' + strArr[4]
+    state = strArr[5]
+    zipCode = strArr[6]
+    console.log('strArr:', strArr)
 
-  // const stateRegEx = /d/
-  // state = str.match(stateRegEx)
+    outputArr.push(streetNumber)
+    outputArr.push(streetName)
+    outputArr.push(cityName)
+    outputArr.push(state)
+    outputArr.push(zipCode)
 
-  // const zipCodeRegEx = /d/
-  // zipCode = str.match(zipCodeRegEx)
-
-  // ADD TO ARRAY
-  outputArr.push(streetNumber, streetName, cityName, state, zipCode)
+  }
 
   // RETURN ARRAY
-
-  // console.log('output:', outputArr)
-
+  console.log('str:', str)
+  console.log('output:', outputArr)
   return outputArr
 }
+
+
 
 // Examples
 decomposeAddress('557 Farmer Rd Corner, MT 59105')
@@ -70,3 +131,8 @@ decomposeAddress('557 Farmer Rd Corner, MT 59105')
 
 // decomposeAddress('8919 Scarecrow Ct Idaho Falls, ID 80193')
 // ➞ ["8919", "Scarecrow Ct", "Idaho Falls", "ID", "80193"]
+
+
+
+// const streetNumberRegExDelete = /((.*)T(.*)\..*",'$1 $2')/
+// streetNumberDelete = str.match(streetNumberRegExDelete)
